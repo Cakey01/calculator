2d5f4cafa4d7a88351eeb2ad6f1665170e0a0c68
Cakey01 <alexanderwarren7@gmail.com>
2025-08-26 14:26:01 -0400

    Create a function to display inputs from the user

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const buttons = document.querySelectorAll("button");
+// get button id when button is pressed
 
 function buttonPress(buttons) {
+    // define input display
+    const display = document.querySelector(".input");
+    // for each button add listener for click
     buttons.forEach(button => {
         button.addEventListener("click", () => {
-            return button.id;
+            // if button clicked isn't clear, backspace, or equals sign, show in the display
+            if (button.id != "clear" && button.id != "backspace" && button.id != "equals") {
+                // if button is an operator, add space around it
+                if (button.id === "÷" || button.id === "x" || button.id === "-" || button.id === "+") {
+                    display.textContent += " " + button.id + " ";
+                } else {
+                    display.textContent += button.id;
+                }
+            }
         });
     });
 }
-console.log(buttonPress(buttons));
+
+
+buttonPress(buttons);
