28ec1ed623c4cb013f1fa4ae029be263ac27a7f1
Cakey01 <alexanderwarren7@gmail.com>
2025-08-29 12:28:33 -0400

    Move clear, backspace, and equals to the beginning of the buttonPress function and add code for backspace case

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll("button");
 const operators = ["-", "+", "x", "÷"];
 const action = ["backspace", "clear", "equals"];
-// get button id when button is pressed
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 function buttonPress(buttons) {
     // define input display
@@ -9,19 +9,36 @@ function buttonPress(buttons) {
     // for each button add listener for click
     buttons.forEach(button => {
         button.addEventListener("click", () => {
-            // if button clicked isn't clear, backspace, or equals sign, show in the display
-            if (!action.includes(button.id)) {
-                // if button is an operator, add space around it
-                if (operators.includes(button.id)) {
-                    display.textContent += " " + button.id + " ";
+            // store current display and last input as variables
+            let currentDisplay = display.textContent;
+            let lastInput = currentDisplay[currentDisplay.length - 1];
+            console.log("current", currentDisplay);
+            console.log("last", lastInput);
+
+            // if button clicked is clear, backspace, or equals, complete those processes
+            if (button.id === "clear") {
+                display.textContent = "";
+            } else if (button.id === "backspace") {
+                // if there is a number before the last char, only remove one char
+                if (numbers.includes(lastInput)) {
+                    display.textContent = currentDisplay.slice(0, currentDisplay.length - 1);
+                }
+                // if there is a space before the last char, remove char and space
+                if (currentDisplay[currentDisplay.length - 2] === " ") {
+                    display.textContent = currentDisplay.slice(0, currentDisplay.length - 2);
+                }
+            } else if (button.id === "equals") {
+                console.log("equals");
+            } else if (numbers.includes(button.id)) {
+                // if last input was an operator add space
+                if (operators.includes(lastInput)) {
+                    display.textContent += " " + button.id;
                 } else {
                     display.textContent += button.id;
                 }
-            // clear display if user presses clear button
-            } else if (button.id === "clear") {
-                display.textContent = "";
-            } else if (button.id === "backspace") {
-
+            } else if (operators.includes(button.id) && numbers.includes(lastInput)) {
+                // if last input was a number add space (cant have two operators in a row)
+                display.textContent += " " + button.id;
             }
         });
     });
